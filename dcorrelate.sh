d77631402af0ea5b4372d62952ba7a1a5b60513b
Sanchangon <sanchangon.ch@gmail.com>
2016-07-15 09:52:32 +0700

    add d.correlate

---
+++ dcorrelate.sh
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# chingchai humhong (chingchai sanchangon)
+# 15/07/2016
+# chingchai.h@gmail.com, chingchaih@nu.ac.th
+# GISTNU @ Naresuan University
+# MIT@License
+# github : https://github.com/sanchangon
+#-------------------------------------------------------------------------------
+# Using Correlation in GRASS GIS 6.x and & 7.x
+# Reference
+https://grass.osgeo.org/grass73/manuals/d.mon.html
+https://grass.osgeo.org/grass70/manuals/d.correlate.html
+https://grass.osgeo.org/grass64/manuals/d.correlate.html
+
+# Set windows for display
+d.mon start=x0   #--> grass 6
+d.mon start=wx0  #--> grass 7
+
+# Set region workspace
+g.region rast=fill.srtm ras=30 -p
+
+# d.correlate for version 6
+g.region rast=lsat7_2002_20
+d.correlate -t layer1=lsat7_2002_20 layer2=lsat7_2002_30 --verbose 
+
+# d.correlate for version 7
+g.region raster=lsat7_2002_30 -p
+d.correlate map=lsat7_2002_30,lsat7_2002_40
