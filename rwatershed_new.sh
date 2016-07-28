d5ac7f1a06e45061cf54fcbce397d59196af2cf4
sanchangon <sanchangon.ch@gmail.com>
2016-07-29 03:24:43 +0700

    add stream order and export shapefile in version1

--- rwatershed_new.sh
+++ rwatershed_new.sh
@@ -25,3 +25,15 @@ r.watershed -f --overwrite elevation=fill.aster@PERMANENT accumulation=rwsh.acc.
 r.watershed -f --overwrite elevation=fill.aw3d.2@PERMANENT accumulation=rwsh.acc.aw3d10 drainage=rwsh.dir.aw3d10 basin=rwsh.bas.aw3d10 stream=rwsh.stm.aw3d10 threshold=10
 r.watershed -f --overwrite elevation=fill.srtm@PERMANENT accumulation=rwsh.acc.srtm10 drainage=rwsh.dir.srtm10 basin=rwsh.bas.srtm10 stream=rwsh.stm.srtm10 threshold=10
 r.watershed -f --overwrite elevation=fuse.f3@PERMANENT accumulation=rwsh.acc.fuse10 drainage=rwsh.dir.fuse10 basin=rwsh.bas.fuse10 stream=rwsh.stm.fuse10 threshold=10
+
+r.stream.order --overwrite stream_rast=rwsh.stm.srtm10@PERMANENT direction=rwsh.dir.srtm10@PERMANENT elevation=fill.srtm@PERMANENT accumulation=rwsh.acc.srtm10@PERMANENT stream_vect=vstream_srtm strahler=order.srtm.strahler
+r.stream.order --overwrite stream_rast=rwsh.stm.aw3d10@PERMANENT direction=rwsh.dir.aw3d10@PERMANENT elevation=fill.aw3d.2@PERMANENT accumulation=rwsh.acc.aw3d10@PERMANENT stream_vect=vstream_aw3d strahler=order.aw3d.strahler
+r.stream.order --overwrite stream_rast=rwsh.stm.aster10@PERMANENT direction=rwsh.dir.aster10@PERMANENT elevation=fill.aster@PERMANENT accumulation=rwsh.acc.aster10@PERMANENT stream_vect=vstream_aster strahler=order.aster.strahler
+r.stream.order --overwrite stream_rast=rwsh.stm.fuse10@PERMANENT direction=rwsh.dir.fuse10@PERMANENT elevation=fuse.f3@PERMANENT accumulation=rwsh.acc.fuse10@PERMANENT stream_vect=vstream_fuse strahler=order.fuse.strahler
+
+r.stream.order --overwrite stream_rast=rwsh.stm.moac100@PERMANENT direction=rwsh.dir.moac100@PERMANENT elevation=ref.moac.fill@PERMANENT accumulation=rwsh.acc.moac100@PERMANENT stream_vect=vstream_moac strahler=order.moac.strahler
+
+
+v.out.ogr input=vstream_aw3d@PERMANENT type=line,auto output=C:\Workspace\Stream_Network_ACRS2016\3-Result format=ESRI_Shapefile
+v.out.ogr input=vstream_fuse@PERMANENT type=line,auto output=C:\Workspace\Stream_Network_ACRS2016\3-Result format=ESRI_Shapefile
+v.out.ogr input=vstream_srtm@PERMANENT type=line,auto output=C:\Workspace\Stream_Network_ACRS2016\3-Result format=ESRI_Shapefile
