f41ccbb09e75b038d3deaf6aa7871131b01beb56
chingchai <chingchai.h@gmail.com>
2017-03-24 11:10:12 +0700

    add slope, aspect and roughness

--- 0-fusion_dem.sh
+++ 0-fusion_dem.sh
@@ -27,8 +27,34 @@ r.relief input=aw3d_ave@PERMANENT output=aw3d_ave.hshed
 r.geomorphon --overwrite elevation=aw3d_ave@PERMANENT forms=aw3d_ave.geomph search=6 skip=0 flat=1 dist=0
 d.shade shade=aw3d_ave.hshed@PERMANENT color=aw3d_ave.geomph@PERMANENT brighten=30
 
-# GDEM
+# ASTER GDEM
 g.region -p raster=gdem@PERMANENT
 r.relief input=gdem@PERMANENT output=gdem.hshed
 r.geomorphon --overwrite elevation=gdem@PERMANENT forms=gdem.geomph search=6 skip=0 flat=1 dist=0
 d.shade shade=gdem.hshed@PERMANENT color=gdem.geomph@PERMANENT brighten=30
+
+# r.slope.aspect - Generates raster maps of slope, aspect, curvatures and partial derivatives from an elevation raster map. Aspect is calculated counterclockwise from east
+# SRTM DEM (degrees , FCELL)
+g.region -p raster=srtm@PERMANENT
+r.slope.aspect --overwrite elevation=srtm@PERMANENT slope=srtm.slope aspect=srtm.aspect
+
+# AW3D DEM
+g.region -p raster=aw3d_ave@PERMANENT
+r.slope.aspect --overwrite elevation=aw3d_ave@PERMANENT slope=aw3d_ave.slope aspect=aw3d_ave.aspect
+
+# ASTER GDEM
+g.region -p raster=gdem@PERMANENT
+r.slope.aspect --overwrite elevation=gdem@PERMANENT slope=gdem.slope aspect=gdem.aspect
+
+# r.roughness.vector 
+# SRTM DEM
+g.region -p raster=srtm@PERMANENT
+r.roughness.vector elevation=srtm@PERMANENT slope=srtm.slope@PERMANENT aspect=srtm.aspect@PERMANENT strength=srtm.strength fisher=srtm.fisher
+
+# AW3D DEM
+g.region -p raster=aw3d_ave@PERMANENT
+r.roughness.vector elevation=aw3d_ave@PERMANENT slope=aw3d_ave.slope@PERMANENT aspect=aw3d_ave.aspect@PERMANENT strength=aw3d_ave.strength fisher=aw3d_ave.fisher
+
+# ASTER GDEM
+g.region -p raster=gdem@PERMANENT
+r.roughness.vector elevation=gdem@PERMANENT slope=gdem.slope@PERMANENT aspect=gdem.aspect@PERMANENT strength=gdem.strength fisher=gdem.fisher
